94ccd6429cc6259d6c4289d289c2a34f52711128
allyytc <allyten8@gmail.com>
2025-10-18 17:11:17 -0700

    updated extension UI

--- pawmodoro-dub/vite.config.ts
+++ pawmodoro-dub/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()
+  ],
+  build:{
+    rollupOptions:{
+      input:{
+        main: resolve(__dirname, 'index.html'),
+        popup: resolve(__dirname, 'public/popup.html'),
+        sidepanel: resolve(__dirname, 'public/sidepanel.html')
+      }
+    }
+  }
 })
